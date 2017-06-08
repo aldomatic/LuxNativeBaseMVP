8716c17e3b17b39a5b820bf66faa46032f15d28f
Aldo Lugo <lugomatic@gmail.com>
2017-06-08 15:50:23 -0500

    updatd themes

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
+import { Image, Text, View, TouchableHighlight } from 'react-native';
 import {
   Container,
   Content,
   Button,
-  Text,
   List,
   ListItem,
+  Item,
   Icon } from 'native-base';
 
 const routes = ["Home", "Camera", "Tour"];
@@ -14,33 +14,46 @@ export default class Menu extends React.Component {
 
 render(){
   return (
-    <Container style={{flex: 1, backgroundColor: '#fff'}}>
-      <Content>
-      <Image
-         source={{
-           uri: "https://github.com/GeekyAnts/NativeBase-KitchenSink/raw/react-navigation/img/drawer-cover.png"
-         }}
-         style={{
-           height: 120,
-           alignSelf: "stretch",
-           justifyContent: "center",
-           alignItems: "center"
-         }}
-       />
+    <Container style={{flex: 1, backgroundColor: '#2A2111'}}>
+      <View>
+      <View style={{
+          height: 100,
+          alignSelf: "stretch",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#393020"
+        }}>
+          <Text style={{
+              color:'#fff',
+              fontSize: 18,
+              fontWeight: 'bold'
+          }}>MENU</Text>
+        </View>
        <List
         dataArray={routes}
         renderRow={data => {
           return (
-            <ListItem
-              button
+            <TouchableHighlight
               onPress={() => this.props.navigation.navigate(data, {})}
+              style={{
+                paddingLeft:25,
+                paddingTop:15,
+                paddingBottom:15,
+                borderBottomColor: '#393020',
+                borderBottomWidth: 1
+              }}
+              underlayColor={'#393020'}
             >
-              <Text>{data}</Text>
-            </ListItem>
+              <Text style={{
+                color:'#fff',
+                fontSize: 16,
+                fontWeight: 'bold'
+              }}>{data}</Text>
+            </TouchableHighlight>
           );
         }}
       />
-      </Content>
+      </View>
     </Container>
   )
 }
